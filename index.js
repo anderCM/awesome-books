edb6387f8518ab89d541bade7b97a73f6230cefa
anderCM2 <ginoander_5@hotmail.com>
2023-01-09 18:00:39 -0500

    Display and remove Books added

--- index.js
+++ index.js
@@ -1,65 +1,76 @@
-const bookForm = document.getElementById("books");
-bookForm.addEventListener("submit", (e) => {
+let books = [];
+
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+const setStorage = (books) => {
+  const dataBook = JSON.stringify(books);
+  localStorage.setItem('data-books', dataBook);
+};
+
+const removeBook = (title, author) => {
+  books = books.filter((book) => book.title !== title || book.author !== author);
+};
+
+const displayBook = (book) => {
+  const booksContainer = document.getElementById('books-container');
+
+  const bookCard = document.createElement('div');
+  bookCard.style.borderBottom = '1px solid #000';
+
+  const pTitle = document.createElement('p');
+  pTitle.textContent = book.title;
+
+  const pAuthor = document.createElement('p');
+  pAuthor.textContent = book.author;
+
+  const removeBtn = document.createElement('button');
+  removeBtn.textContent = 'Remove';
+
+  bookCard.append(pTitle, pAuthor, removeBtn);
+  booksContainer.appendChild(bookCard);
+
+  removeBtn.addEventListener('click', () => {
+    removeBook(book.title, book.author);
+    setStorage(books);
+    booksContainer.removeChild(bookCard);
+  });
+};
+
+const iterateBooks = () => {
+  books.map((book) => displayBook(book));
+};
+
+const saveBooks = () => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+
+  const book = new Book(title, author);
+  books.push(book);
+  setStorage(books);
+  displayBook(book);
+};
+
+const bookForm = document.getElementById('books');
+
+bookForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const idbook=Math.floor(Math.random() * 100);
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  /*const booksContainer = document.getElementById('books-container');*/
-
-  const localData = localStorage.getItem("books-data");
-  let currentData;
-  if (localData) {
-    currentData = JSON.parse(localData);
-  } else {
-    currentData = [];
-  }
-
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
   if (!title || !author) {
-    alert("Please provide a Title or Author");
+    alert('Please enter a Title and Author first');
     return;
   }
-
-  currentData.push({ idbook,title, author });
-  localStorage.setItem("books-data", JSON.stringify(currentData));
-  console.log(currentData);
-  display(currentData);
+  saveBooks();
+  title.value = '';
+  author.value = '';
 });
-let currentData_1=localStorage.getItem("books-data");
-currentData_1 = JSON.parse(currentData_1);
 
-function display(currentData){
-  const booksContainer = document.getElementById('books-container');
-  currentData.map((book) => {
-    
-    const titlBook = book.title;
-    const authorBook = book.author;
-    const id=book.idbook;
-    const bookCard = document.createElement('div');
-    bookCard.id=id;
-    bookCard.style.borderBottom = '1px solid #000';
-    
-    const pTitle = document.createElement('p');
-    pTitle.textContent = titlBook;
-
-    const pAuthor = document.createElement('p');
-    pAuthor.textContent = authorBook;
-
-    const removeBtn = document.createElement('button');
-    removeBtn.textContent = 'Remove';
-    removeBtn.addEventListener("click",()=>{ removebtn(currentData,id)});
-
-    bookCard.append(pTitle, pAuthor,removeBtn);
-    booksContainer.appendChild(bookCard);
-  });
+if (localStorage.getItem('data-books')) {
+  const totalBooks = JSON.parse(localStorage.getItem('data-books'));
+  books = totalBooks;
+  console.log(books);
 }
-
-display(currentData_1);
-
-function removebtn(currentData,id){
-const cardElement=document.getElementById(id);
-cardElement.remove();
-const dellocal=currentData.filter((book) => {
-return book.idbook != id});
-localStorage.setItem("books-data",JSON.stringify(dellocal));
-console.log(dellocal);
-}
+iterateBooks();
